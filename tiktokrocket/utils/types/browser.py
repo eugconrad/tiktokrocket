4b51ff906dc53b0e0069e817600003ff1da015d8
eugconrad <eugconrad@icloud.com>
2025-02-12 00:27:37 +0200

    Refactor Browser class: extracted proxy and user agent handling into static methods, simplified proxy parsing, added docstrings, and refined Chrome options setup

--- tiktokrocket/utils/types/browser.py
+++ tiktokrocket/utils/types/browser.py
@@ -12,13 +12,16 @@ Social: @eugconrad – Telegram, Lolzteam
         © Copyright 2024
 """
 import os
-
 from fake_useragent import UserAgent
 from seleniumwire import undetected_chromedriver as uc
 from selenium_stealth import stealth
 
 
 class Browser:
+    """
+    Class for managing browser interactions using Selenium and undetected_chromedriver.
+    """
+
     headless: bool
     proxy: dict | None
     user_agent: str
@@ -26,10 +29,22 @@ class Browser:
     sw_options: dict
     driver: uc
 
-    def __init__(self):
-        pass
+    def create(
+            self,
+            browser_path: str,
+            headless: bool = False,
+            proxy: str = None,
+            user_agent: str = None
+    ):
+        """
+        Creates and configures a new browser instance with specified settings.
 
-    def create(self, browser_path: str, headless: bool = False, proxy: str = None, user_agent: str = None):
+        Args:
+            browser_path (str): Path to the browser executable and driver.
+            headless (bool): Whether to run the browser in headless mode.
+            proxy (str, optional): Proxy server address with optional authentication.
+            user_agent (str, optional): Custom user agent string.
+        """
         # --- Browser path ---
         driver_executable_path = os.path.join(browser_path, 'chromedriver.exe')
         browser_executable_path = os.path.join(browser_path, 'chrome.exe')
@@ -38,37 +53,23 @@ class Browser:
         self.headless = headless
 
         # --- Proxy ---
-        self.proxy = None
-        if proxy:
-            self.proxy = {"server": f"http://{proxy}"}
-            if "@" in proxy:
-                self.proxy["server"] = f"http://{proxy.split('@')[1]}"
-                self.proxy["username"] = proxy.split("@")[0].split(":")[0]
-                self.proxy["password"] = proxy.split("@")[0].split(":")[1]
+        self.proxy = self._get_proxy(proxy)
 
         # --- User agent ---
-        if user_agent:
-            self.user_agent = user_agent
-        else:
-            ua = UserAgent(browsers=["chrome"], os=["windows"], platforms=["pc"])
-            self.user_agent = ua.random
-        if self.user_agent.endswith(" "):
-            self.user_agent = self.user_agent[:-1]
+        self.user_agent = self._get_user_agent(user_agent)
 
         # --- Chrome options ---
         self.options = uc.ChromeOptions()
-        self.options.add_argument(f"--user-agent={user_agent}")
+        self.options.add_argument(f"--user-agent={self.user_agent}")
 
         # Set Chrome options for better automation experience
-        self.options.add_argument("--disable-popup-blocking")  # Disable any popups that may interrupt
+        self.options.add_argument("--disable-popup-blocking")
         self.options.add_experimental_option("prefs", {
             "profile.default_content_setting_values.popups": 1,
             "profile.default_content_setting_values.notifications": 1,
-            # "profile.managed_default_content_settings.images": 2,
-            # "profile.managed_default_content_settings.css": 2,
         })
 
-        # Additional Chrome self.options to optimize performance and stability
+        # Additional Chrome options to optimize performance and stability
         self.options.add_argument("--disable-background-networking")
         self.options.add_argument("--disable-background-timer-throttling")
         self.options.add_argument("--disable-backgrounding-occluded-windows")
@@ -83,17 +84,17 @@ class Browser:
         self.options.add_argument("--safebrowsing-disable-auto-update")
         self.options.add_argument("--password-store=basic")
         self.options.add_argument("--use-mock-keychain")
-        self.options.add_argument("--disable-infobars")  # Disable annoying info bars in Chrome
-        self.options.add_argument("--disable-blink-features=AutomationControlled")  # Avoid detection as a bot
-        self.options.add_argument("--no-sandbox")  # Fix issues in some environments
-        self.options.add_argument("--disable-dev-shm-usage")  # Handle resource issues
-        self.options.add_argument("--disable-gpu")  # Disable GPU acceleration
+        self.options.add_argument("--disable-infobars")
+        self.options.add_argument("--disable-blink-features=AutomationControlled")
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument("--disable-dev-shm-usage")
+        self.options.add_argument("--disable-gpu")
 
         self.options.add_argument("--ignore-certificate-errors")
         self.options.add_argument("--disable-extensions")
 
         # --- Selenium wire options ---
-        self.sw_options = dict()
+        self.sw_options = {}
         self.sw_options['verify_ssl'] = False
         if self.proxy:
             self.sw_options['proxy'] = self.proxy
@@ -120,10 +121,33 @@ class Browser:
         self.driver.maximize_window()
         self.driver.implicitly_wait(0.5)
 
+    @staticmethod
+    def _get_proxy(proxy):
+        if proxy:
+            proxy_parts = proxy.split("@")
+            proxy_data = {"server": f"http://{proxy_parts[-1]}"}
+            if len(proxy_parts) > 1:
+                username, password = proxy_parts[0].split(":")
+                proxy_data.update({"username": username, "password": password})
+            return proxy_data
+        return None
+
+    @staticmethod
+    def _get_user_agent(user_agent):
+        if user_agent:
+            return user_agent.rstrip()
+        return UserAgent(browsers=["chrome"], os=["windows"], platforms=["pc"]).random
+
     def reset(self):
+        """
+        Resets the browser session by clearing cookies and storage.
+        """
         self.driver.delete_all_cookies()
         self.driver.execute_script("window.localStorage.clear();")
         self.driver.execute_script("window.sessionStorage.clear();")
 
     def quit(self):
+        """
+        Quits the browser session and closes the browser window.
+        """
         self.driver.quit()
